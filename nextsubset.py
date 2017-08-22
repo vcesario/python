2c43d9b26852637e4a20f2b35bcbfc49dc5c7d0f
victor cesário <vcesario@users.noreply.github.com>
2017-08-21 21:42:51 -0300

    add description and test to nextsubset.py

--- nextsubset.py
+++ nextsubset.py
@@ -1,3 +1,14 @@
+'''
+	[nextsubset.py]
+	This function takes two lists as parameters, where the first one is a subset of the second one.
+	It then calculates the next subset of the same length within the given set.
+	E.g.: in the superset [1, 2, 3, 4, 5, 6]
+		- the first subset of size 3 is [1, 2, 3]
+		- the last subset of size 3 is [4, 5, 6]
+
+	author: github.com/vcesario
+'''
+
 def nextSubset(subset, superset):
 	size = len(subset);
 	SIZE = len(superset);
@@ -20,6 +31,12 @@ def nextSubset(subset, superset):
 	
 	return None;		
 
-subset = [1, 2, 6];
 superset = [1, 2, 3, 4, 5, 6];
-print('after ' + str(subset) + ' comes ' + str(nextSubset(subset, superset)));
+
+subset = [1, 2, 6];
+nxt = nextSubset(subset, superset);
+assert(nxt == [1, 3, 4]); #True
+
+subset = [4, 5, 6];
+nxt = nextSubset(subset, superset);
+assert(nxt == None); #True
